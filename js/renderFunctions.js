630caf1fe295af808ce74ea4f5b0d311c8d5d9e6
a1astair <alastairbeaumont@protonmail.com>
2020-02-12 16:17:51 -0800

    changing p to item

--- js/renderFunctions.js
+++ js/renderFunctions.js
@@ -5,18 +5,18 @@ import {
     View,
   } from 'react-native';
 import { Image } from 'react-native-elements'
-export const renderTouchables = (key, imageSource, p, styles) => {
+export const renderTouchables = (key, imageSource, item, styles) => {
     return (
     <View key={key}>
-      <TouchableHighlight key={key} underlayColor='#fff' onPress={() => this.showElement(p)}>
+      <TouchableHighlight key={key} underlayColor='#fff' onPress={() => this.showElement(item)}>
         <Image
           key={key}
           style={styles.image}
           source={imageSource}
         />
       </TouchableHighlight>
-      <Text>Caption: {p.caption}</Text>
-      <Text>Timestamp: {new Date(p.timestamp).toDateString()}</Text>
+      <Text>Caption: {item.caption}</Text>
+      <Text>Timestamp: {new Date(item.timestamp).toDateString()}</Text>
     </View>
     )
   }
